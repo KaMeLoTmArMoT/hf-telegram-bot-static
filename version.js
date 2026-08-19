394dbc8b84559a4072c2be0524c8b00eb57a7b21
KaMeLoTmArMoT <KaMeLoTmArMoT@users.noreply.github.com>
2026-08-19 22:37:40 +0000

    deploy: KaMeLoTmArMoT/hf-telegram-bot@ceda0b20d8e488d1a1b35e3386e23c461de1d560

---
+++ version.js
@@ -0,0 +1,5 @@
+// Auto-generated version file. Do not edit manually.
+window.APP_VERSION = "2026.08.19-2237";
+window.COMMIT_HASH = "e2bdcdc";
+window.BUILD_DATE = "2026-08-19 22:37";
+window.FULL_VERSION = "2026.08.19-2237 (e2bdcdc)";
